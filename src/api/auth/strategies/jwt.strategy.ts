dd2ea71e1dc7bd02c7c69d4969361379bd65ea1b
BeSocial <118454057+besochill@users.noreply.github.com>
2022-12-22 21:21:35 +0000

    Update jwt.strategy.ts

--- src/api/auth/strategies/jwt.strategy.ts
+++ src/api/auth/strategies/jwt.strategy.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { AuthService } from '../auth.service';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly authService: AuthService) {
+  constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: process.env.JWT_ACCESS_TOKEN_SECRET,
     });
+  }
+
+  async validate(payload: any) {
+    return { userId: payload.sub, username: payload.username };
   }
 }
